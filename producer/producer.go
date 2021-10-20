ccb87e834d0588b88d8c87e01d5daed5f6d358c7
Iovesophy <kazu825@me.com>
2021-10-20 16:50:10 +0900

    ADD producer: sample flow of producer

--- producer/producer.go
+++ producer/producer.go
@@ -1,17 +1,30 @@
 package producer
 
-import "fmt"
-
-var (
-	_ Producer = (*NewProducer)(nil)
+import (
+	"fmt"
 )
 
-type NewProducer struct{}
+var (
+	_ Producer = (*NewSampleProducer)(nil)
+)
 
+// Producer interface list
 type Producer interface {
-	CreateProducer()
+	CreateSampleProducer(int)
 }
 
-func (p *NewProducer) CreateProducer() {
-	fmt.Println("producer")
+// Producer Prams of Value
+type NewSampleProducer struct {
+	Msgs *chan int
+	Done *chan bool
+}
+
+// Set Visibility-Flow of Producer sending chan value for test
+func (p *NewSampleProducer) CreateSampleProducer(max int) {
+	fmt.Println("start producer")
+	for ch := 0; ch < max; ch++ {
+		fmt.Println("produce: Sending ", ch)
+		*p.Msgs <- ch
+	}
+	*p.Done <- true
 }
